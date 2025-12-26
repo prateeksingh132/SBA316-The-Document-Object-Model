560cb2daa3bbd4d259b352e26eaecf15f31b5c71
prateeksingh132 <kumarpks132@gmail.com>
2025-12-25 20:36:48 -0800

    Starting out, creating the project structure

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,16 @@
 
 // Objectives
 
+// You will create a small single-page web application.
 // Use DOM properties, methods, and techniques to create a web application that provides a dynamic user experience.
 // Use BOM properties, methods, and techniques to facilitate creation of a dynamic web application.
 // Demonstrate profi ciency with event-driven programming and DOM events.
 // Implement basic form validation using any combination of built-in HTML validation attributes and DOM-event-driven JavaScript validation.
+
+//////// WHAT I KNOW:
+// Its supposed to be a single page website
+// Combine topics of HTML/CSS and Javascript/DOM/BOM
+// 
 
 
 //////////////////////////////////// SBA 316
