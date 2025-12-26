66f97f10270a1e5b80be5869278f0915042777a6
prateeksingh132 <kumarpks132@gmail.com>
2025-12-25 20:05:55 -0800

    Starting out, creating the project structure

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
 
+// Objectives
+
+// Use DOM properties, methods, and techniques to create a web application that provides a dynamic user experience.
+// Use BOM properties, methods, and techniques to facilitate creation of a dynamic web application.
+// Demonstrate profi ciency with event-driven programming and DOM events.
+// Implement basic form validation using any combination of built-in HTML validation attributes and DOM-event-driven JavaScript validation.
+
+
 //////////////////////////////////// SBA 316
 
 console.log("\n");
